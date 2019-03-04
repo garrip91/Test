e418ff386c81faddc4dd0bb9b431f76a6a3b82f9
garrip91 <45920127+garrip91@users.noreply.github.com>
2019-03-05 02:00:18 +0300

    2. PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<?php 
+
+	// Функция "isCorrect($param)" (для проверки корректности каждой строки согласно условию), в которой параметр "$param" будет использоваться в качестве примера:
+  	function isCorrect($param) {
+		$stack = [];
+		$symbols = array('[' => ']', '{' => '}', '(' => ')');
+
+		foreach(str_split($param) as $s) {
+			if(array_key_exists($s, $symbols)) {
+				$stack[] = $s;
+				continue;
+			}  elseif(array_search($s, $symbols) == array_pop($stack)) {
+				  continue;
+			   }
+			return false;
+		}
+		
+		return true;
+	} 
+	
+//	var_dump(assert(isCorrect('') === true));
+//	var_dump(assert(isCorrect('()') === true));
+//	var_dump(assert(isCorrect('{(})[]') === false));
+
+?>
